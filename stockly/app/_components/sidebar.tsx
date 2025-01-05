c07e4266e41e425a614d6eb71d5dbf06d30c5f86
Uberlei Brito <uberleidasilva@gmail.com>
2025-01-05 14:26:01 -0300

    refactor: update sidebar styles and integrate Prettier with Tailwind CSS support

--- stockly/app/_components/sidebar.tsx
+++ stockly/app/_components/sidebar.tsx
@@ -1,8 +1,8 @@
 const SideBar = () => {
   return (
     <div className="w-64 bg-white">
-      <div className="px-6 py-6 ">
-        <h1 className="font-bold text-2xl">STOCKLY</h1>
+      <div className="px-8 py-6">
+        <h1 className="text-2xl font-bold">STOCKLY</h1>
       </div>
       <div className="flex flex-col gap-2 p-2">
         <button className="px-6 py-3">Dashboard</button>
