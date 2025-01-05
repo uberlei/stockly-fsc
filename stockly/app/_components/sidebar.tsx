bcd1923ff150f9dc5db76f7d7c6a64861dd4fb9e
Uberlei Brito <uberleidasilva@gmail.com>
2025-01-05 14:11:13 -0300

    Sidebar

---
+++ stockly/app/_components/sidebar.tsx
@@ -0,0 +1,16 @@
+const SideBar = () => {
+  return (
+    <div className="w-64 bg-white">
+      <div className="px-6 py-6 ">
+        <h1 className="font-bold text-2xl">STOCKLY</h1>
+      </div>
+      <div className="flex flex-col gap-2 p-2">
+        <button className="px-6 py-3">Dashboard</button>
+        <button className="px-6 py-3">Produtos</button>
+        <button className="px-6 py-3">Vendas</button>
+      </div>
+    </div>
+  );
+};
+
+export default SideBar;
