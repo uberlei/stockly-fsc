7d66414b36ecea67c814310d9310ee99aebdbafd
Uberlei Brito <uberleidasilva@gmail.com>
2025-01-14 10:46:33 -0300

    feat: add server-only dependency and refactor product data fetching

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,11 +1,11 @@
 import { PlusIcon } from "lucide-react";
 import { Button } from "../_components/ui/button";
-import { db } from "../_lib/prisma";
 import { DataTable } from "../_components/ui/data-table";
 import { productTableColumns } from "./_components/table-columns";
+import { getProduct } from "../_data-acess/product/get-product";
 
 const Products = async () => {
-  const products = await db.product.findMany({});
+  const products = await getProduct();
 
   return (
     <div className="m-8 w-full space-y-8 rounded-lg bg-white p-8 px-8">
