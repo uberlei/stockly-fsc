5ab97a8e835fabc13e08a954816bdf31dcef4ea7
Uberlei Brito <uberleidasilva@gmail.com>
2025-01-21 18:40:37 -0300

    refactor: clean up product-table-columns.tsx by removing unused imports and components to streamline code

--- app/products/_components/product-table-columns.tsx
+++ app/products/_components/product-table-columns.tsx
@@ -1,30 +1,9 @@
 "use client";
 
-import {
-  AlertDialog,
-  AlertDialogTrigger,
-} from "@/app/_components/ui/alert-dialog";
 import { Badge } from "@/app/_components/ui/badge";
-import { Button } from "@/app/_components/ui/button";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@/app/_components/ui/dropdown-menu";
 import { Product } from "@prisma/client";
 import { ColumnDef } from "@tanstack/react-table";
-import {
-  CircleIcon,
-  ClipboardListIcon,
-  EditIcon,
-  MoreHorizontalIcon,
-  TrashIcon,
-} from "lucide-react";
-import DeleteProductDialogContent from "./delete-product-dialog-content";
-
-import UpsertProductDialogContent from "./upsert-product-dialog-content";
-import { useState } from "react";
+import { CircleIcon } from "lucide-react";
 import ProductTableDropdownMenu from "./product-table-dropdown-menu";
 
 const getStatusLabel = (status: string) => {
