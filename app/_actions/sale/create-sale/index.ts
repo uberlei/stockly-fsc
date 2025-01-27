1e1817953918527bfc7b630c6b858ba9105d4e70
Uberlei Brito <uberleidasilva@gmail.com>
2025-01-27 20:31:27 -0300

    feat: refactor createSale function to use transactions; add stock validation and ensure product availability during sale creation

--- app/_actions/sale/create-sale/index.ts
+++ app/_actions/sale/create-sale/index.ts
@@ -7,38 +7,42 @@ import { revalidatePath } from "next/cache";
 export const createSale = async (data: CreateSaleSchema) => {
     createSaleShema.parse(data);
 
-    const sale = await db.sale.create({
-        data: {
-            date: new Date(),
-        },
+    await db.$transaction(async (trx) => {
+
+        const sale = await trx.sale.create({
+            data: {
+                date: new Date(),
+            },
+        });
+
+        for (const product of data.products) {
+            const productFromDB = await db.product.findUnique({
+                where: {
+                    id: product.id,
+                },
+            });
+
+            if (!productFromDB) {
+                throw new Error(`Product with id ${product.id} not found`);
+            }
+
+            if (productFromDB.stock < product.quantity) {
+                throw new Error(`Product with id ${product.id} has insufficient stock`);
+            }
+
+            await trx.saleProduct.create({
+                data: {
+                    saleId: sale.id,
+                    productId: product.id,
+                    quantity: product.quantity,
+                    unitPrice: productFromDB.price,
+                },
+            });
+
+            await trx.product.update({ where: { id: product.id }, data: { stock: { decrement: product.quantity } } });
+        }
     });
 
-    for (const product of data.products) {
-        const productFromDB = await db.product.findUnique({
-            where: {
-                id: product.id,
-            },
-        });
-
-        if (!productFromDB) {
-            throw new Error(`Product with id ${product.id} not found`);
-        }
-
-        if (productFromDB.stock < product.quantity) {
-            throw new Error(`Product with id ${product.id} has insufficient stock`);
-        }
-
-        await db.saleProduct.create({
-            data: {
-                saleId: sale.id,
-                productId: product.id,
-                quantity: product.quantity,
-                unitPrice: productFromDB.price,
-            },
-        });
-
-        await db.product.update({ where: { id: product.id }, data: { stock: { decrement: product.quantity } } });
-    }
     revalidatePath("/sales");
     revalidatePath("/products");
 }
